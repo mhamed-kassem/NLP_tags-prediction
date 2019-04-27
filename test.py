ff4fc1b0430ea3c85c511781869aa5cc05702956
محمد ابوالقاسم سيد محمد <mhamed_kassem@yahoo.com>
2019-04-27 23:17:20 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -11,6 +11,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB,MultinomialNB
 from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 
 data = pd.read_csv('stack-overflow-data.csv')
 df = pd.DataFrame(data)
@@ -32,9 +34,12 @@ X_train, X_test, y_train, y_test = train_test_split(X_train_tfidf, y, test_size=
 
 
 clf = MultinomialNB().fit(X_train, y_train)
+y_predict = clf.predict(X_test)
 score = clf.score(X_test,y_test)
-print('MultinomialNB SCORE : ',score)
 
+print('MultinomialNB SCORE : ',score)
+print(confusion_matrix(y_test,y_predict))
+print(classification_report(y_test, y_predict))
 
 
 '''
@@ -55,8 +60,11 @@ X_train, X_test, y_train, y_test = train_test_split(features, y, test_size=0.33,
 
 gnb = GaussianNB()
 gnb.fit(X_train,y_train)
+y_predict = gnb.predict(X_test)
 score = gnb.score(X_test,y_test)
 print('GaussianNB SCORE : ',score)
+print(confusion_matrix(y_test,y_predict))
+print(classification_report(y_test, y_predict))
 
 
 '''
